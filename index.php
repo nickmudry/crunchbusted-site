e669d3b6fbcf7d4d3ad80cdd6a82846477779333
Nick Mudry <angryfacing@gmail.com>
2017-03-19 14:20:20 -0400

    changes

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <html>
-<body>
+<head>
+<title>#CrunchBusted</title>
+<meta name=”description” content="The greatest prank to ever happen at PAX East 2017.">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42417402-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<META NAME="ROBOTS" CONTENT="INDEX, FOLLOW">
+<meta http-equiv="refresh" content="140;url=http://www.mudrysmozz.net/" />
+
+</head>
+<body align="center" style="background-color:black; padding-top:5%;">
+
+<iframe width="1280" height="720" src="https://www.youtube.com/embed/uHC-Tv74358?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>
 
 </body>
 </html>
